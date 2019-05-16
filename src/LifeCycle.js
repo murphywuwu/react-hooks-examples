17a55ac4e677fbace6389f148eb611dfb6188404
murphywuwu <murphywuwu@gmail.com>
2019-05-16 23:13:53 +0800

    💡 useEffect: 卸载与加载组件时的调用行为

--- src/LifeCycle.js
+++ src/LifeCycle.js
@@ -3,8 +3,8 @@ import React, { useEffect, useState } from 'react';
 function LifeCycleDemo () {
   useEffect(() => {
     console.log('render');
-
-    // useEffect不仅仅在组件uumounted时会调用，它也会在每次effect调用前被调用
+    
+    // useEffect不仅仅在组件unmounted时会调用，它也会在每次render调用前被调用(useEffect的默认行为)
     // 这实际上比componentWillUnmount生命周期更有力量，因为它可以让你在每次render前后，都执行一次副作用
     return () => console.log('unmounting');
   });
@@ -22,7 +22,7 @@ function LifeCycle () {
 
   return (
     <>
-       {/* 点击Re-render按钮，先答应unmounting，再打印render */}
+       {/* 点击Re-render按钮，先打印unmounting，再打印render */}
       <button onClick={reRender}>Re-render</button>
       {/* 单击show/hide按钮，rendder和unmounting交替打印 */}
       <button onClick={toggle}>Show/Hide LifecycleDemo</button>
