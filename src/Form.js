c66de3eeacbcdfe24c041d860b7c5413622279d5
murphywuwu <murphywuwu@gmail.com>
2019-05-16 23:35:09 +0800

    💡 useEffect: 组件卸载与初次加载以及组件重渲染时的调用行为

--- src/Form.js
+++ src/Form.js
@@ -10,10 +10,14 @@ function Form() {
   //   console.log(name + ' ' + surname)
   //   document.title = name + ' ' + surname
   // })
-  // 当name变量变化时或首次渲染，回调函数被调用 
+  // 首次渲染，会打印打印name + ' ' + surname，但不会打印unmouting
+  // 每当name变量的值发生变化时，都会导致组件重渲染
+  // 而每当组件重渲染，会先打印unmouting,再打印name + ' ' + surname
   useEffect(() => {
     console.log(name + ' ' + surname)
     document.title = name + ' ' + surname
+
+    return () => console.log('unmounting');
   }, [name])
 
   // name或surname变化时或首次渲染，回调函数被调用
--- src/LifeCycle.js
+++ src/LifeCycle.js
@@ -6,6 +6,7 @@ function LifeCycleDemo () {
     
     // useEffect不仅仅在组件unmounted时会调用，它也会在每次render调用前被调用(useEffect的默认行为)
     // 这实际上比componentWillUnmount生命周期更有力量，因为它可以让你在每次render前后，都执行一次副作用
+    // 组件卸载时，只会打印一次unmounting, 但不会打印render
     return () => console.log('unmounting');
   });
 
