545aedcacc9279431f968352a687ca973c30797f
murphywuwu <murphywuwu@gmail.com>
2019-05-17 13:33:34 +0800

    🚧 (Example.js) useEeffect&&useLayoutEffect调用顺序

--- src/Example.js
+++ src/Example.js
@@ -1,57 +1,26 @@
-// https://juejin.im/post/5bfa929551882524cb6f413b#heading-14
-import React from 'react';
+import React, { useEffect, useLayoutEffect } from 'react'
 
-let state = [];
-let setters = [];
-let firstRun = true;
-let cursor = 0;
-
-function createSetter(cursor) {
-  return function setterWithCursor(newVal) {
-    state[cursor] = newVal;
-  }
-}
-
-export function useState(initVal) {
-  if (firstRun) {
-    state.push(initVal);
-    setters.push(createSetter(cursor));
-    firstRun = false;
-  }
-
-  const setter = setters[cursor];
-  const value = state[cursor];
-
-  cursor++;
-  return [value, setter];
-}
+function Example () {
 
 
-function RenderFunctionComponent() {
-  const [firstName, setFirstName] = useState("Rudi");
-  const [lastName, setLastName] = useState("Yardley");
+  useEffect(() => {
+    console.log('use effect lifecycle: component did mount');
+  }, [])
+
+  useLayoutEffect(() => {
+    console.log('use layout effect lifecycle: component did mount');
+  }, [])
+
+  console.log('render lifecycle');
 
   return (
     <>
-      <button onClick={() => setFirstName("Fred")}>Fred</button>
-      <p>{firstName} {lastName}</p>
+      <div>
+        <label>Your name</label>
+        <input type="text" autoComplete="off"/>
+      </div>
     </>
-  );
+  )
 }
 
-// This is sort of simulating Reacts rendering cycle
-function MyComponent() {
-  cursor = 0; // resetting the cursor
-  return <RenderFunctionComponent />; // render
-}
-
-console.log(state); // Pre-render: []
-MyComponent();
-console.log(state); // First-render: ['Rudi', 'Yardley']
-MyComponent();
-console.log(state); // Subsequent-render: ['Rudi', 'Yardley']
-
-// click the 'Fred' Button
-console.log(state); // After-click: ['Fred', 'Yardley']
-
-export default MyComponent;
+export default Example;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import App from "./App";
 import Form from './Form';
 import LifeCycle from './LifeCycle';
 import Reddit from './Reddit';
+import Example from './Example';
 import * as serviceWorker from "./serviceWorker";
 
 // https://daveceddia.com/useeffect-hook-examples/
-ReactDOM.render(<Form/>, document.getElementById("root"));
+ReactDOM.render(<Example/>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
