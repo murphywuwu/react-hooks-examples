51813926f23e40c33fbcf846903f1067dc1c2142
murphywuwu <murphywuwu@gmail.com>
2019-05-17 18:08:02 +0800

    🚧 (useEffect-example) useEffect: 避免内存泄漏

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import App from "./useState-example";
+import App from './useEffect-example';
 import Form from './Form';
 import LifeCycle from './LifeCycle';
 import Reddit from './Reddit';
@@ -8,7 +9,7 @@ import Example from './Example';
 import * as serviceWorker from "./serviceWorker";
 
 // https://daveceddia.com/useeffect-hook-examples/
-ReactDOM.render(<Example/>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
