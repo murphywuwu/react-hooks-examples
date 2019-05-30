5ded2b798a7d9fbfdba8606876307ef893293487
murphywuwu <murphywuwu@gmail.com>
2019-05-30 13:17:48 +0800

    🚧 (Example.js) Don't Lie to React About Dependencies
    https://overreacted.io/a-complete-guide-to-useeffect/

--- src/Example.js
+++ src/Example.js
@@ -1,26 +1,31 @@
 import React, { useEffect, useState, useRef } from 'react'
 import styles from './App.css'
 
+// function Example () {
+//   const [count, setCount] = useState(0);
+
+//   useEffect(() => {
+//     const id = setInterval(() => {
+//       setCount(count + 1);
+//     }, 1000);
+
+//     return () => clearInterval(id);
+//   }, []);
+
+//   return <h1>{count}</h1>
+// }
 function Example () {
   const [count, setCount] = useState(0);
-  const latestCount = useRef(count);
 
   useEffect(() => {
-    latestCount.current = count;
+    const id = setInterval(() => {
+      setCount(c => c + 1);
+    }, 1000);
 
-    setTimeout(() => {
-      console.log(`You clicked ${latestCount.current} times`);
-    }, 3000);
-  })
+    return () => clearInterval(id);
+  }, []);
 
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
+  return <h1>{count}</h1>
 }
 
 export default Example;
