f1472fe94b8eb515d010cfd6b9fa16b9f3c8cd92
murphywuwu <murphywuwu@gmail.com>
2019-05-30 10:36:21 +0800

    🚧 (Example.js) Swimming Against the Tide
    https://overreacted.io/a-complete-guide-to-useeffect/

--- src/Example.js
+++ src/Example.js
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import styles from './App.css'
 
 function Example () {
   const [count, setCount] = useState(0);
+  const latestCount = useRef(count);
 
-  function handleAlertClick() {
+  useEffect(() => {
+    latestCount.current = count;
+
     setTimeout(() => {
-      alert('You clicked on: ' + count);
+      console.log(`You clicked ${latestCount.current} times`);
     }, 3000);
-  }
+  })
 
   return (
     <div>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>
         Click me
-      </button>
-      <button onClick={handleAlertClick}>
-        Show alert
       </button>
     </div>
   );
